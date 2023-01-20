f30279588e4aff5e6afcb21f8bac5860a3d0c31d
trotsky98 <109166384+trotsky98@users.noreply.github.com>
2023-01-20 15:58:46 +0300

    Fix

--- HW5/terminal/TerminalReader.java
+++ HW5/terminal/TerminalReader.java
@@ -1,5 +1,5 @@
 package terminal;
-//только один экземпляр terminalReader
+
 
 import repository.StudentRepository;
 import service.StudentService;
@@ -13,26 +13,26 @@ import view.TerminalCmdView;
 import java.util.Scanner;
 
 public class TerminalReader {
-    private static TerminalReader terminalReader;//приватный статичный экземпляр класса
-    private final CommandParser commandParser;//реализуем метод интерфейса через экземпляр класса для парсинга команд
+    private static TerminalReader terminalReader;
+    private final CommandParser commandParser;
 
 
     private TerminalReader(CommandParser commandParser) {
         this.commandParser = commandParser;
-    }//приватный конструктор
+    }
 
     public static TerminalReader getInstance(CommandParser commandParser) {
         if (terminalReader == null) {
             terminalReader = new TerminalReader(commandParser);
         }
         return terminalReader;
-    }//Фабричный метод
+    }
     CommandExecutableFactory commandExecutableFactoryImpl = new LoggingCommandExecutableFactoryImpl(new StudentService(new StudentRepository()),new StudentView(),new TerminalCmdView());//logging
     public void listen() {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String cmd = sc.nextLine();
-            Command cmds = commandParser.parseCommand(cmd);//парсинг команд
+            Command cmds = commandParser.parseCommand(cmd);
             CommandExecutable commandExecutable = commandExecutableFactoryImpl.create(cmds);
             if (commandExecutable != null) {
                 commandExecutable.execute();
