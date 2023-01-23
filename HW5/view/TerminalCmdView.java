788e6f957fb3d011caa06823510f60c01053746c
trotsky98 <109166384+trotsky98@users.noreply.github.com>
2023-01-23 19:04:18 +0300

    Solid

--- HW5/view/TerminalCmdView.java
+++ HW5/view/TerminalCmdView.java
@@ -2,7 +2,7 @@ package view;
 
 public class TerminalCmdView {
     public void cr() {
-        System.out.println(" created");
+        System.out.println("status: created");
     }
     public void rm() {
         System.out.println(" removed");
