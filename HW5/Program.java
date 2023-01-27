e64ae956bc51b5069c29a68ae3770741eebd85e5
Andrew <varyag8484@mail.ru>
2023-01-27 16:24:02 +0300

    fix HW7

--- HW5/Program.java
+++ HW5/Program.java
@@ -4,10 +4,10 @@ import terminal.executable.Command;
 
 import java.util.List;
 
-//String firstName, String lastName, int id, String department, int yearOfBirth, int course, int groupNumber
+
 public class Program{
     public static void main(String[] args) {
-        //String[] testStudent =new String[]{"cr","Ivan","Pumpkin","321","GTF","1986","3","321"};
+        
         TerminalReader.getInstance(new CommandParser() {
             @Override
             public Command parseCommand(String inputCommand) {
