5ba47760b42ebf2edece872d42ca406c37059e8b
trotsky98 <109166384+trotsky98@users.noreply.github.com>
2023-01-27 17:16:35 +0300

    fixx

--- HW7/terminal/TerminalReader.java
+++ HW7/terminal/TerminalReader.java
@@ -1,5 +1,5 @@
 package terminal;
-//только один экземпляр terminalReader
+
 
 import terminal.executable.Command;
 import terminal.executable.CommandExecutable;
@@ -11,22 +11,22 @@ import view.LoggingCommandExecutableFactoryImplView;
 import java.util.Scanner;
 
 public class TerminalReader extends CommandView {
-    private static TerminalReader terminalReader;//приватный статичный экземпляр класса
-    private final CommandParser commandParser;//реализуем метод интерфейса через экземпляр класса для парсинга команд
-    private final CommandExecutableFactory commandExecutableFactory;//абстракция для вызова метода
+    private static TerminalReader terminalReader;
+    private final CommandParser commandParser;
+    private final CommandExecutableFactory commandExecutableFactory;
 
 
     private TerminalReader() {
         commandParser = new CommandParserImpl();//реализация парсера
-        commandExecutableFactory = new LoggingCommandExecutableFactoryImpl(new LoggingCommandExecutableFactoryImplView());//реализация фабрики
-    }//приватный конструктор
+        commandExecutableFactory = new LoggingCommandExecutableFactoryImpl(new LoggingCommandExecutableFactoryImplView());
+    }
 
     public static TerminalReader getInstance() {
         if (terminalReader == null) {
             terminalReader = new TerminalReader();
         }
         return terminalReader;
-    }//Фабричный метод
+    }
 
     public void listen() {
         Scanner sc = new Scanner(System.in);
@@ -36,7 +36,7 @@ public class TerminalReader extends CommandView {
             cmd = sc.nextLine();
             if (cmd.equals("exit"))
                 break;
-            Command cmds = commandParser.parseCommand(cmd);//парсинг команд
+            Command cmds = commandParser.parseCommand(cmd);
             CommandExecutable commandExecutable = commandExecutableFactory.create(cmds);
             if (commandExecutable != null)
                 commandExecutable.execute();
